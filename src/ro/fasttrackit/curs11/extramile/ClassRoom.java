76987325436ef7a2bbe981dda79a1a35c699a25b
Cristina <tepelea.denisa@yahoo.ro>
2020-02-17 19:45:57 +0200

    read Students created Classroom

---
+++ src/ro/fasttrackit/curs11/extramile/ClassRoom.java
@@ -0,0 +1,23 @@
+package ro.fasttrackit.curs11.extramile;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ClassRoom {
+    private final List<StudentGrade> students;
+
+    public ClassRoom(List<StudentGrade> students) {
+        this.students = new ArrayList<>(students);
+    }
+
+    public List<Integer> getGrades(String discipline) {
+        List<Integer> result = new ArrayList<>();
+        for (StudentGrade student : students) {
+            if (student.getDiscipline().equals(discipline)) {
+                result.add(student.getGrade());
+            }
+
+        }
+        return result;
+    }
+}
